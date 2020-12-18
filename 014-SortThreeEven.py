a7accd39fd6032bd00785e8a7eca1ffc0c8881e4
Fangbaso4 <44719631+Fangbaso4@users.noreply.github.com>
2020-12-18 17:39:52 +0800

    014

---
+++ 014-SortThreeEven.py
@@ -0,0 +1,17 @@
+def sort_even(n):
+    even_list = []
+    count = 0
+    while len(even_list) < n:
+        print ("请输入一个整数")
+        num = int(input("> "))
+        count += 1
+        if num%2 == 0:
+            print(f"你输入的数字是{num},是偶数")
+            even_list.append(num)
+        else:
+            print(f"你输入的数字是{num},不是偶数")
+    print("你一共输入了{}个数字,其中{}个为偶数，分别是：".format(count,n),end="")
+    even_list.sort()
+    print(even_list)
+
+sort_even(3)
